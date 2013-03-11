a4423d26a1efda07b4e7ce8a1dda2df1f99dafe2
James <e@mailjames.com>
2013-03-11 19:09:46 +0000

    rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,19 +1,18 @@
-task :install => [:npm]
+task :install => [:npm, :createPublicFolder]
 task :default => [:runProgram]
-task :build => [:stopService, :npm, :startService]
 
 
 task :npm do
     sh "npm install"
 end
 
-task :stopService do
-    sh "echo <<pass>> | sudo service <<ServiceName>> stop"
+task :createPublicFolder do
+	Dir.chdir("app") do
+		Dir::mkdir("public")
+	end
 end
 
-task :startService do
-    sh "echo <<pass>> | sudo service <<ServiceName>> start"
-end
+
 
 task :runProgram do
   sh "node index.js"
