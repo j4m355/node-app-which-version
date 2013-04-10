8d33ef20ca22828c2806734117c0e08e34f845c4
James <e@mailjames.com>
2013-04-10 22:45:51 +0100

    Rake

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,15 +1,18 @@
-task :install => [:npm, :createPublicFolder]
+task :install => [:npm]
+task :build => [:stopService, :npm, :startService]
 task :default => [:runProgram]
 
 
 task :npm do
-    sh "npm install"
+    sh "echo downfall | sudo -S npm install"
 end
 
-task :createPublicFolder do
-	Dir.chdir("app") do
-		Dir::mkdir("public")
-	end
+task :stopService do
+    sh "echo downfall | sudo -S service worker-kue stop"
+end
+
+task :startService do
+    sh "echo downfall | sudo -S service worker-kue start"
 end
 
 task :runProgram do
